c9ad124f1d95ebe37f40098aee55c8922fd132cf
Jasmine Lee and Micaela Jawor <jasmine@>
2019-03-27 16:33:35 -0700

    fix 3rd test

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import './styles.css';
+import $ from 'jquery';
+import  HungryBear  from './../src/hungrybear.js';
+
+$(document).ready(function(){
+  $('#hangryBearImage').hide()
+  $('#bearJustAteYouImage').hide()
+  $('#happySatiatedBearImage').hide()
+  let name = "Grizzly";
+  let newHungryBear = new HungryBear(name);
+  newHungryBear.setHunger();
+  setInterval(function(){
+    if (newHungryBear.didYouGetEaten() == false){
+      $('#foodLevel').text(newHungryBear.foodLevel);
+    } else {
+      newHungryBear.clearInterval();
+    }
+
+    if (0 < newHungryBear.foodLevel && newHungryBear.foodLevel <= 5) {
+      $('#happySatiatedBearImage').hide();
+      $('#hangryBearImage').show();
+      $('#bearLabel').text("Bear is hungry");
+    }
+    else if (newHungryBear.foodLevel == 0) {
+      $('#hangryBearImage').hide();
+      $('#happySatiatedBearImage').hide();
+      $('#bearJustAteYouImage').show();
+      $('#bearLabel').text("Bear is just ate you");
+    }
+    else {
+      $('#bearJustAteYouImage').hide();
+      $('#hangryBearImage').hide();
+      $('#happySatiatedBearImage').show();
+      $('#bearLabel').text("Bear is happy");
+    }
+  }, 1000);
+
+  $('#run').submit(function(event){
+    event.preventDefault();
+    newHungryBear.feed();
+  });
+});
